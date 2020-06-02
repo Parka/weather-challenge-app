b168a733aad7ad7e29342d9ba591a79f5e3d73ed
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-06-02 17:27:30 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Weather from './features/weather/';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
